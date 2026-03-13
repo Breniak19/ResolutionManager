9bdad3b773e356a3f2bde63b9af6febe9372e3a2
Breniak19 <mixaelaranguren@gmail.com>
2026-03-13 01:51:46 -0400

    Refactor Form1.Designer.cs layout and controls

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,9 @@
-﻿namespace ResolutionManager
+namespace ResolutionManager
 {
     partial class Form1
     {
-        /// <summary>
-        /// Variable del diseñador necesaria.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Limpiar los recursos que se estén usando.
-        /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,12 +13,6 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
-
-        /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
@@ -38,140 +25,137 @@
             this.txtNombreProceso = new System.Windows.Forms.TextBox();
             this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.mostrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnActivarDesactivar = new System.Windows.Forms.Button();
+            this.numHz = new System.Windows.Forms.NumericUpDown();
+            this.lblHz = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numAncho)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numAlto)).BeginInit();
-            this.trayMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numHz)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAgregar
             // 
-            this.btnAgregar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.btnAgregar.FlatAppearance.BorderSize = 3;
-            this.btnAgregar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAgregar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.btnAgregar.Location = new System.Drawing.Point(194, 70);
-            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAgregar.BackColor = System.Drawing.Color.DimGray;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.ForeColor = System.Drawing.Color.Lime;
+            this.btnAgregar.Location = new System.Drawing.Point(194, 50);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(88, 28);
+            this.btnAgregar.Size = new System.Drawing.Size(100, 30);
             this.btnAgregar.TabIndex = 0;
             this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.UseVisualStyleBackColor = false;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // btnEliminar
             // 
-            this.btnEliminar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.btnEliminar.FlatAppearance.BorderSize = 3;
-            this.btnEliminar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnEliminar.Location = new System.Drawing.Point(194, 122);
-            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEliminar.BackColor = System.Drawing.Color.DimGray;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.ForeColor = System.Drawing.Color.Red;
+            this.btnEliminar.Location = new System.Drawing.Point(194, 86);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(88, 28);
+            this.btnEliminar.Size = new System.Drawing.Size(100, 30);
             this.btnEliminar.TabIndex = 1;
             this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.UseVisualStyleBackColor = false;
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // lstProcesos
             // 
-            this.lstProcesos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lstProcesos.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstProcesos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.lstProcesos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstProcesos.ForeColor = System.Drawing.Color.White;
             this.lstProcesos.FormattingEnabled = true;
             this.lstProcesos.ItemHeight = 16;
-            this.lstProcesos.Location = new System.Drawing.Point(14, 15);
-            this.lstProcesos.Margin = new System.Windows.Forms.Padding(4);
+            this.lstProcesos.Location = new System.Drawing.Point(12, 12);
             this.lstProcesos.Name = "lstProcesos";
-            this.lstProcesos.Size = new System.Drawing.Size(140, 98);
+            this.lstProcesos.Size = new System.Drawing.Size(170, 144);
             this.lstProcesos.TabIndex = 2;
             // 
             // numAncho
             // 
-            this.numAncho.Location = new System.Drawing.Point(14, 206);
-            this.numAncho.Margin = new System.Windows.Forms.Padding(4);
+            this.numAncho.Location = new System.Drawing.Point(12, 185);
             this.numAncho.Name = "numAncho";
-            this.numAncho.Size = new System.Drawing.Size(128, 23);
+            this.numAncho.Size = new System.Drawing.Size(70, 23);
             this.numAncho.TabIndex = 3;
             // 
             // numAlto
             // 
-            this.numAlto.Location = new System.Drawing.Point(171, 206);
-            this.numAlto.Margin = new System.Windows.Forms.Padding(4);
+            this.numAlto.Location = new System.Drawing.Point(105, 185);
             this.numAlto.Name = "numAlto";
-            this.numAlto.Size = new System.Drawing.Size(110, 23);
+            this.numAlto.Size = new System.Drawing.Size(70, 23);
             this.numAlto.TabIndex = 4;
             // 
             // txtNombreProceso
             // 
             this.txtNombreProceso.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtNombreProceso.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNombreProceso.Location = new System.Drawing.Point(174, 15);
-            this.txtNombreProceso.Margin = new System.Windows.Forms.Padding(4);
-            this.txtNombreProceso.MaxLength = 64;
+            this.txtNombreProceso.ForeColor = System.Drawing.Color.Gray;
+            this.txtNombreProceso.Location = new System.Drawing.Point(194, 12);
             this.txtNombreProceso.Name = "txtNombreProceso";
-            this.txtNombreProceso.Size = new System.Drawing.Size(116, 24);
+            this.txtNombreProceso.Size = new System.Drawing.Size(100, 23);
             this.txtNombreProceso.TabIndex = 5;
-            this.txtNombreProceso.Tag = "";
+            this.txtNombreProceso.Text = "proceso";
             this.txtNombreProceso.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtNombreProceso.Enter += new System.EventHandler(this.txtNombreProceso_Enter);
             this.txtNombreProceso.Leave += new System.EventHandler(this.txtNombreProceso_Leave);
             // 
-            // trayIcon
-            // 
-            this.trayIcon.ContextMenuStrip = this.trayMenu;
-            this.trayIcon.Text = "notifyIcon1";
-            this.trayIcon.Visible = true;
-            // 
-            // trayMenu
-            // 
-            this.trayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.mostrarToolStripMenuItem,
-            this.salirToolStripMenuItem});
-            this.trayMenu.Name = "trayMenu";
-            this.trayMenu.Size = new System.Drawing.Size(116, 48);
-            // 
-            // mostrarToolStripMenuItem
-            // 
-            this.mostrarToolStripMenuItem.Name = "mostrarToolStripMenuItem";
-            this.mostrarToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
-            this.mostrarToolStripMenuItem.Text = "Mostrar";
-            // 
-            // salirToolStripMenuItem
-            // 
-            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
-            this.salirToolStripMenuItem.Text = "Salir";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(149, 208);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(88, 187);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(15, 16);
             this.label1.TabIndex = 6;
-            this.label1.Text = "X";
+            this.label1.Text = "x";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(11, 256);
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(12, 260);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 16);
             this.label2.TabIndex = 7;
             this.label2.Text = "By Breniak";
             // 
+            // btnActivarDesactivar
+            // 
+            this.btnActivarDesactivar.BackColor = System.Drawing.Color.LightGreen;
+            this.btnActivarDesactivar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnActivarDesactivar.Location = new System.Drawing.Point(194, 122);
+            this.btnActivarDesactivar.Name = "btnActivarDesactivar";
+            this.btnActivarDesactivar.Size = new System.Drawing.Size(100, 34);
+            this.btnActivarDesactivar.TabIndex = 8;
+            this.btnActivarDesactivar.Text = "Desactivar";
+            this.btnActivarDesactivar.UseVisualStyleBackColor = false;
+            this.btnActivarDesactivar.Click += new System.EventHandler(this.btnActivarDesactivar_Click);
+            // 
+            // numHz
+            // 
+            this.numHz.Location = new System.Drawing.Point(12, 225);
+            this.numHz.Name = "numHz";
+            this.numHz.Size = new System.Drawing.Size(70, 23);
+            this.numHz.TabIndex = 9;
+            // 
+            // lblHz
+            // 
+            this.lblHz.AutoSize = true;
+            this.lblHz.ForeColor = System.Drawing.Color.White;
+            this.lblHz.Location = new System.Drawing.Point(88, 227);
+            this.lblHz.Name = "lblHz";
+            this.lblHz.Size = new System.Drawing.Size(22, 16);
+            this.lblHz.TabIndex = 10;
+            this.lblHz.Text = "Hz";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::ResolutionManager.Properties.Resources.RMInterfaz;
-            this.ClientSize = new System.Drawing.Size(340, 293);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(320, 285);
+            this.Controls.Add(this.lblHz);
+            this.Controls.Add(this.numHz);
+            this.Controls.Add(this.btnActivarDesactivar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtNombreProceso);
@@ -180,22 +164,20 @@
             this.Controls.Add(this.lstProcesos);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnAgregar);
-            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "ResolutionManager Breniak";
+            this.Text = "Resolution Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.numAncho)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numAlto)).EndInit();
-            this.trayMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numHz)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
-
-        #endregion
 
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnEliminar;
@@ -205,10 +187,10 @@
         private System.Windows.Forms.TextBox txtNombreProceso;
         private System.Windows.Forms.NotifyIcon trayIcon;
         private System.Windows.Forms.ContextMenuStrip trayMenu;
-        private System.Windows.Forms.ToolStripMenuItem mostrarToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnActivarDesactivar;
+        private System.Windows.Forms.NumericUpDown numHz;
+        private System.Windows.Forms.Label lblHz;
     }
 }
-
